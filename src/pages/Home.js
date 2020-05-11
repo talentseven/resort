f4b28d6dcd5839752c0fafb00302ce8b8a141c29
tz <tianzi1007@gmail.com>
2020-05-11 17:07:04 -0400

    update details in SingelRooms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import Hero from "../Components/Hero"
 import Banner from '../Components/Banner'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Services from '../Components/Services'
 import FeatureRooms from '../Components/FeatureRooms'
 import { RoomContext } from '../Context';
+import Button from '../Components/StyledHero'
 
-export default function Home () {
+
+export default function Home() {
     return (
-            <>
-                <Hero>
-                    <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-                        <Link to="/rooms" className="btn-primary">
-                            our rooms
+        <>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn-primary">
+                        our rooms
                         </Link>
-                    </Banner>
-                </Hero>
-                <Services></Services>
-                <FeatureRooms></FeatureRooms>
-            </> 
-        );
+                </Banner>
+            </Hero>
+            <Services></Services>
+            <FeatureRooms></FeatureRooms>
+            <Button> Hello </Button>
+        </>
+    );
 }
 
 
